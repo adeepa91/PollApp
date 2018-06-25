2764ed20b60da73c2779ceec427b77c12300415c
Adeepa Koonkaduwa <koonade@cacorp.com>
2018-06-25 14:44:09 +0530

    Added ServerError files

---
+++ src/common/ServerError.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../common/ServerError.css';
+
+class ServerError extends Component {
+  render() {
+    return (
+      <p>hi ServerError</p>
+    );
+  }
+}
+
+export default ServerError;
